24d105553dc2112a72001a761234ff7baf4bee7a
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-11-21 15:14:15 +0530

    Completed UI.

--- src/components/Folder.js
+++ src/components/Folder.js
@@ -2,6 +2,25 @@ import React, { useState } from "react";
 
 const Folder = ({ explorer }) => {
   const [expand, setExpand] = useState(false);
+  const [showInput, setShowInput] = useState({
+    visible: false,
+    isFolder: null,
+  });
+
+  const handleNewFolder = (e, isFolder) => {
+    e.stopPropagation();
+    setExpand(true);
+    setShowInput({
+      visible: true,
+      isFolder,
+    });
+  };
+
+  const onAddFolder = (e) => {
+    if (e.keyCode === 13 && e.target.value) {
+      setShowInput({ ...showInput, visible: false });
+    }
+  };
 
   if (explorer.isFolder) {
     return (
@@ -10,12 +29,24 @@ const Folder = ({ explorer }) => {
           <span>📁 {explorer.name}</span>
 
           <div>
-            <button>Folder +</button>
-            <button>File +</button>
+            <button onClick={(e) => handleNewFolder(e, true)}>Folder +</button>
+            <button onClick={(e) => handleNewFolder(e, false)}>File +</button>
           </div>
         </div>
 
         <div style={{ display: expand ? "block" : "none", paddingLeft: 25 }}>
+          {showInput.visible && (
+            <div className="inputContainer">
+              <span>{showInput.isFolder ? "📁" : "📄"}</span>
+              <input
+                type="text"
+                onKeyDown={onAddFolder}
+                onBlur={() => setShowInput({ ...showInput, visible: false })}
+                className="inputContainer__input"
+                autoFocus
+              />
+            </div>
+          )}
           {explorer.items.map((exp) => {
             return <Folder explorer={exp} key={exp.id} />;
           })}
